ce9537d808f89bf7578eb791dc6c6f15eb8a715f
Yukiniro <yukiniro@hotmail.com>
2024-08-17 16:46:36 +0800

    feat: test fibonacci

--- hello-wasm/hello_world.c
+++ hello-wasm/hello_world.c
@@ -7,7 +7,38 @@
 
 #include <stdio.h>
 
+
+// 实现斐波那契函数
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+#include <time.h>
+
+// 计算斐波那契函数的耗时并打印
+void print_fibonacci_time(int n) {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    int result = fibonacci(n);
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("计算斐波那契数列第 %d 个数的耗时: %f 秒\n", n, cpu_time_used);
+    printf("结果: %d\n", result);
+}
+
+// 在主函数中调用斐波那契函数并打印结果
 int main() {
-  printf("hello, world!\n");
-  return 0;
+    printf("hello, world!\n");
+    
+    int n = 42;
+    // printf("斐波那契数列的第 %d 个数是: %d\n", n, fibonacci(n));
+    print_fibonacci_time(n);
+    
+    return 0;
 }
